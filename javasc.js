408c62ec72fd93e8ccb877aba0e6a23e16403fb7
aakruti27 <52568409+aakruti27@users.noreply.github.com>
2020-03-25 22:45:44 +0500

    java script new

---
+++ javasc.js
@@ -0,0 +1,18 @@
+function dochange()
+{
+	k=confirm("aakruti loves you");
+	if (k==true) {
+		alert("ok means you forgave me hehe thenks")
+	}
+	else
+	{
+		alert("you broke my heart :(")
+	}
+}
+function chgtxt()
+{
+	var del1= document.getElementById("d1");
+	var del2= document.getElementById("d2");
+	del1.innerHTML="TEEEEXT 1";
+	del2.innerHTML="TEEEEXT 2";
+}
